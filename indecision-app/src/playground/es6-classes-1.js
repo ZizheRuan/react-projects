aaa784f92cdc5058cf6e98bc1f3a6d5c0a8dabe3
ZizheRuan <ruanzizhe@live.com>
2019-01-19 23:03:18 +1100

    2
    2

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,6 +1,16 @@
 class Person {
+    //if just 'age', the default is Undefined, 
+    // but there must be an 'age' instead of nothing.
+    constructor(name = 'Anonymous', age = 0){ 
+        this.name = name;
+        this.age = age;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} years old`;
+    } 
 
 }
 
-const me = new Person();
-console.log(me);
+const me = new Person('Andrew Mead','26');
+console.log(me.getDescription());
