3f4c59b57ad5331779e14734714f5d958e5caa1d
ZizheRuan <ruanzizhe@live.com>
2019-01-19 23:45:01 +1100

    3

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -12,5 +12,17 @@ class Person {
 
 }
 
-const me = new Person('Andrew Mead','26');
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);//let parent class do their job & define their items.
+        this.major = major;
+    }
+}
+
+
+const me = new Student('Andrew Mead','26','Computer Science');
+console.log(me);
 console.log(me.getDescription());
+const other = new Student();
+console.log(other);
+console.log(other.getDescription());
