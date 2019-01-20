70086ad121d357dce849bb98ab8697a5ca2158a0
ZizheRuan <ruanzizhe@live.com>
2019-01-20 22:35:54 +1100

    lecture 26

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -22,6 +24,11 @@ var Person = function () {
     }
 
     _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
         key: 'getDescription',
         value: function getDescription() {
             return this.name + ' is ' + this.age + ' years old';
@@ -49,17 +56,63 @@ var Student = function (_Person) {
         value: function hasMajor() {
             return !!this.major;
         }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            //complete overriden the same name function in the Parent Class
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+            if (this.hasMajor()) {
+                description += ', Their major is ' + this.major + '.';
+            }
+            return description;
+        }
     }]);
 
     return Student;
 }(Person);
 
-var me = new Student('Andrew Mead', '26', 'Computer Science');
-console.log(me);
-console.log(me.getDescription());
-console.log(me.hasMajor());
+var Traveller = function (_Person2) {
+    _inherits(Traveller, _Person2);
 
-var other = new Student();
+    function Traveller(name, age, homeLocation) {
+        _classCallCheck(this, Traveller);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveller.__proto__ || Object.getPrototypeOf(Traveller)).call(this, name, age));
+
+        _this2.homeLocation = homeLocation;
+        return _this2;
+    }
+
+    _createClass(Traveller, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            var greeting = _get(Traveller.prototype.__proto__ || Object.getPrototypeOf(Traveller.prototype), 'getGreeting', this).call(this);
+            if (this.homeLocation) {
+                greeting += ', I\'m visiting from ' + this.homeLocation;
+            }
+            return greeting;
+        }
+    }]);
+
+    return Traveller;
+}(Person);
+
+// const me = new Student('Andrew Mead','26','Computer Science');
+// console.log(me);
+// console.log(me.getDescription());
+// console.log(me.hasMajor());
+
+// const other = new Student();
+// console.log(other);
+// console.log(other.getDescription());
+// console.log(other.hasMajor());
+
+var me = new Traveller('Andrew Mead', '26', 'Philadelphia');
+console.log(me);
+console.log(me.getGreeting());
+// console.log(me.hasHomeLocation());
+
+var other = new Traveller();
 console.log(other);
-console.log(other.getDescription());
-console.log(other.hasMajor());
+console.log(other.getGreeting());
+// console.log(other.hasHomeLocation());
--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -5,11 +5,12 @@ class Person {
         this.name = name;
         this.age = age;
     }
-
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
     getDescription() {
         return `${this.name} is ${this.age} years old`;
     } 
-
 }
 
 class Student extends Person {
@@ -21,15 +22,49 @@ class Student extends Person {
     hasMajor(){
         return !!this.major;
     }
+
+    getDescription (){
+        //complete overriden the same name function in the Parent Class
+        let description = super.getDescription();
+        if (this.hasMajor()){
+            description += `, Their major is ${this.major}.`;
+        }
+        return description
+    }
+}
+
+class Traveller extends Person {
+    constructor (name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting (){
+        let greeting = super.getGreeting();
+        if (this.homeLocation){
+            greeting += `, I'm visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+
 }
 
 
-const me = new Student('Andrew Mead','26','Computer Science');
-console.log(me);
-console.log(me.getDescription());
-console.log(me.hasMajor());
+// const me = new Student('Andrew Mead','26','Computer Science');
+// console.log(me);
+// console.log(me.getDescription());
+// console.log(me.hasMajor());
 
-const other = new Student();
+// const other = new Student();
+// console.log(other);
+// console.log(other.getDescription());
+// console.log(other.hasMajor());
+
+const me = new Traveller('Andrew Mead','26','Philadelphia');
+console.log(me);
+console.log(me.getGreeting());
+// console.log(me.hasHomeLocation());
+
+const other = new Traveller();
 console.log(other);
-console.log(other.getDescription());
-console.log(other.hasMajor());
+console.log(other.getGreeting());
+// console.log(other.hasHomeLocation());
