40b2c8e947af8a21841bd700f7ba70f724a725a3
ZizheRuan <ruanzizhe@live.com>
2019-01-21 22:17:57 +1100

    L33

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,59 +1,63 @@
-class IndecisionApp extends React.Component{
-    render(){
-        const title = 'Indecision';
-        const subtitle = 'Put your life in the hands of a computer';
-        const options = ['Thing one','Thing two','Thing four'];
+class IndecisionApp extends React.Component {
+    render() {
+      const title = 'Indecision';
+      const subtitle = 'Put your life in the hands of a computer';
+      const options = ['Thing one', 'Thing two', 'Thing four'];
+  
+      return (
+        <div>
+          <Header title={title} subtitle={subtitle} />
+          <Action />
+          <Options options={options} />
+          <AddOption />
+        </div>
+      );
+    }
+  }
+  
+  class Header extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <h2>{this.props.subtitle}</h2>
+        </div>
+      );
+    }
+  }
+  
+  class Action extends React.Component {
+    handlePick() {
+      alert('handlePick');
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handlePick}>What should I do?</button>
+        </div>
+      );
+    }
+  }
 
-        return(
-            <div>
-                <Header title = {title} subtitle = {subtitle} />
-                <Action />
-                <Options options = {options}/>
-                <AddOption />
-            </div>
-        );
-    }
-}
-
-class Header extends React.Component{
-    render (){
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
-}
-
-class Action extends React.Component{
-    handlePick(){
-        alert('handlePick');
-    }
-    render (){
-        return (//just reference to handlePick, not call it, so don't use {this.handlePick()}
-            <div>
-                <button onClick = {this.handlePick}>What should I do?</button>
-            </div>
-        );
-    }
-}
-
-class Options extends React.Component{
-    handleRemove (){
-        alert('handleRemove');
-    }
-    render(){
-        return(
-            <div>
-                <button onClick = {this.handleRemove}>Remove All</button>
-                {//optionText is to pass the option name to its child class "Option"
-                    this.props.options.map((option)=> <Option key={option} optionText = {option}/> )
-                }
-                <Option />
-            </div>
-        );
-    }
+class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll() {
+    console.log(this.props.options);
+    // alert('handleRemoveAll');
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+      </div>
+    );
+  }
 }
 
 class Option extends React.Component{
--- indecision-app/src/playground/build-visible.js
+++ indecision-app/src/playground/build-visible.js
@@ -1,30 +1,58 @@
-let details = "";
-let wordOnButton = "Show details";
+// let details = "";
+// let wordOnButton = "Show details";
 
-const showDetails = () => {
-    if (wordOnButton == "Show details"){
-        wordOnButton = "Show less";
-        details = "details";
+// const showDetails = () => {
+//     if (wordOnButton == "Show details"){
+//         wordOnButton = "Show less";
+//         details = "details";
+//     }
+//     else {
+//         wordOnButton = "Show details";
+//         details = "";
+//     }
+//     renderApp();
+// }
+
+// const appRoot = document.getElementById('app');// can be put in ReactDOM.render...
+
+// const renderApp = () => {
+//     const template = ( //can use  other names other than template
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick = {showDetails}> {wordOnButton} </button>
+//             <p>{details}</p>
+//         </div>
+//     );
+//     ReactDOM.render(template,appRoot);
+// }
+
+// renderApp();
+
+
+
+
+
+
+//define constructor for two reasons: bind methods & set up default state
+class VisibilityToggle extends React.Component {
+    constructor(props){
+        super(props);
+        //binding
+        this.state = {
+            state: false
+        };
     }
-    else {
-        wordOnButton = "Show details";
-        details = "";
+
+    render(){
+        return(
+            <div>
+                <h1>Visibility</h1>
+                <button onClick = {this.handleShowMore}>Show More</button>
+                <button onClick = {this.handleShowLess}>Show Less</button>
+                <p>{this.details}</p>
+            </div>
+        )
     }
-    renderApp();
+
 }
-
-const appRoot = document.getElementById('app');// can be put in ReactDOM.render...
-
-const renderApp = () => {
-    const template = ( //can use  other names other than template
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick = {showDetails}> {wordOnButton} </button>
-            <p>{details}</p>
-        </div>
-    );
-    ReactDOM.render(template,appRoot);
-}
-
-renderApp();
-
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
