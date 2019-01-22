16af0175e90dc03e251b5b5b0af159a08b755f5c
ZizheRuan <ruanzizhe@live.com>
2019-01-22 23:39:53 +1100

    L36 before challege

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,32 @@
 class IndecisionApp extends React.Component {
+    constructor (props){
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.state = {
+            options: ['Thing one', 'Thing two', 'Thing four']
+        }
+    }
+
+    handleDeleteOptions() {
+        this.setState(()=>{
+            return{
+                options: []
+            }
+        })
+    }
+
     render() {
       const title = 'Indecision';
       const subtitle = 'Put your life in the hands of a computer';
-      const options = ['Thing one', 'Thing two', 'Thing four'];
   
       return (
         <div>
           <Header title={title} subtitle={subtitle} />
-          <Action />
-          <Options options={options} />
+          <Action hasOptions={this.state.options.length > 0} />
+          <Options 
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOption} 
+          />
           <AddOption />
         </div>
       );
@@ -40,18 +58,10 @@ class IndecisionApp extends React.Component {
   }
 
 class Options extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-  }
-  handleRemoveAll() {
-    console.log(this.props.options);
-    // alert('handleRemoveAll');
-  }
   render() {
     return (
       <div>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
