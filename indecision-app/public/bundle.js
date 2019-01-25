0e991aee3aea35cb468ee2a9c1228ae4638696be
ZizheRuan <ruanzizhe@live.com>
2019-01-26 00:31:33 +1100

    L54

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -65,9 +65,23 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
 
 console.log('app.js is running');
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+console.log('utils.js is running!');
 
 /***/ })
 /******/ ]);
