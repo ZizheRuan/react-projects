93577f75cf9745b1ec9dcfca62b94629169d780d
ZizheRuan <ruanzizhe@live.com>
2019-03-28 16:46:47 +1100

    basic Detail page completed

--- react_news_site-master/src/js/components/pc_news_details.js
+++ react_news_site-master/src/js/components/pc_news_details.js
@@ -8,7 +8,8 @@ export default class PcNewsDetails extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			newsItem: ''
+			newsItem: '',
+			imageBottom: ''
 		};
 	}
 
@@ -17,13 +18,14 @@ export default class PcNewsDetails extends React.Component {
 			method: 'GET'
 		};
 		// fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
-		fetch("http://app.meljianghu.com/api/activity/get_by_cate/67/1/1@2@3@4@5@6" + this.props.params.uniquekey, myFetchOptions)
+		fetch("http://app.meljianghu.com/api/activity/get_by_cate/67/1/1@2@3@4@5@6", myFetchOptions)
 		.then(response => response.json())
 		.then(json => {
-			this.setState({newsItem: json[0]});
+			this.setState({newsItem: json[2]});
+			this.setState({imageBottom: this.state.newsItem.img_url_bottom.replace('\\','').replace("/home/meljianghu","http:/")});
 			document.title = this.state.newsItem.title + " | 江湖活动平台";
 		})
-		console.log(this.state)
+
 	}
 
 	//将请求到的html结构放入div里
@@ -39,9 +41,15 @@ export default class PcNewsDetails extends React.Component {
 				<Row>
 					<Col span={2}></Col>
 					<Col span={14} className="container">
-						<div className="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
-						<div>details</div>
+						{/*<div className="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>*/}
 						<div>{this.state.newsItem.title}</div>
+						<div>{`浏览${this.state.newsItem.count_views_real}次`}</div>
+						<div>{`$${this.state.newsItem.unit_price}`}</div>
+						<div>{`${this.state.newsItem.address}`}</div>
+						<div>{`${this.state.newsItem.start_time} - ${this.state.newsItem.end_time}`}</div>
+						<div>{`详情：${this.state.newsItem.des}`}</div>
+						<img src={this.state.imageBottom} alt={this.state.group_logo_url} />
+						<div>{this.state.imageBottom}</div>
 					</Col>
 					<Col span={6}>
 						<PcNewsImageBlock count={20} type="top" width="100%" cardTitle="相关新闻" imageWidth="140px" />
