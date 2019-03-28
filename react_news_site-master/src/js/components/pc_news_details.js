2a37f64c13e18f5c4b2646eab361b75f42121a5c
ZizheRuan <ruanzizhe@live.com>
2019-03-28 11:17:10 +1100

    .

--- react_news_site-master/src/js/components/pc_news_details.js
+++ react_news_site-master/src/js/components/pc_news_details.js
@@ -16,17 +16,20 @@ export default class PcNewsDetails extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+		// fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+		fetch("http://app.meljianghu.com/api/activity/get_by_cate/67/1/1@2@3@4@5@6" + this.props.params.uniquekey, myFetchOptions)
 		.then(response => response.json())
 		.then(json => {
-			this.setState({newsItem: json});
-			document.title = this.state.newsItem.title + " - Jianghu Media | 江湖活动平台";
+			this.setState({newsItem: json[0]});
+			document.title = this.state.newsItem.title + " | 江湖活动平台";
 		})
+		console.log(this.state)
 	}
 
 	//将请求到的html结构放入div里
 	createMarkup() {
-		return {__html: this.state.newsItem.pagecontent};
+		// return {__html: this.state.newsItem.pagecontent};
+		return {__html: this.state.newsItem.title};
 	}
 
 	render(){
@@ -38,6 +41,7 @@ export default class PcNewsDetails extends React.Component {
 					<Col span={14} className="container">
 						<div className="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
 						<div>details</div>
+						<div>{this.state.newsItem.title}</div>
 					</Col>
 					<Col span={6}>
 						<PcNewsImageBlock count={20} type="top" width="100%" cardTitle="相关新闻" imageWidth="140px" />
